6451efc249bb274c841d49eac29d8ae5dc78ef97
SirAllex18 <anistor35@yahoo.com>
2024-03-25 22:10:32 +0200

    redux setup for auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage"; 
+
 
 function App() {
+
+
   return (
     <div className="app">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/scenes/navBar/index.jsx
+++ client/src/scenes/navBar/index.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+import {
+  Box,
+  Select,
+  MenuItem,
+  Typography,
+  IconButton,
+  FormControl,
+  useMediaQuerry,
+} from "@mui/material";
+
+import { useDispatch, useSelector } from "react-redux";
+import { setLogout } from "state";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "components/FlexBetween";
 
 const NavBar = () => {
-    return (
-        <div> NavBar </div>
-    )
-}
+  return <div> NavBar </div>;
+};
 
 export default NavBar;
