6993b19c71c9ecfe15b5d087dd66a018be69fd66
SirAllex18 <anistor35@yahoo.com>
2024-05-25 16:57:09 +0300

    add players path

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
+import PlayersPage from "scenes/playersPage"
 import LoginPage from "scenes/loginPage"; 
+
 import { CssBaseline } from "@mui/material";
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/players" element={<PlayersPage /> } />
         </Routes>
       </BrowserRouter>
     </div>
