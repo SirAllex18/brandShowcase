5f5c9ba5f73e5328fde9e960a57e5d05c866b95a
SirAllex18 <anistor35@yahoo.com>
2024-06-11 02:31:47 +0300

    login page layout

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -1,232 +1,3 @@
-// import { useState } from "react";
-// import {
-//   Box,
-//   Button,
-//   TextField,
-//   useMediaQuery,
-// } from "@mui/material";
-
-// import { Formik } from "formik";
-// import * as yup from "yup";
-// import { useNavigate } from "react-router-dom";
-// import { useDispatch } from "react-redux";
-// import { setLogin } from "state";
-
-
-// const registerSchema = yup.object().shape({
-//   firstName: yup.string().required("required"),
-//   lastName: yup.string().required("required"),
-//   password: yup.string().required("required"),
-// });
-
-// const loginSchema = yup.object().shape({
-//   password: yup.string().required("required"),
-// });
-
-
-// const initialValuesRegister = {
-//   firstName: "",
-//   lastName: "",
-//   password: "",
-// };
-
-// const initialValuesLogin = {
-//   password: "",
-// };
-
-// const Form = () => {
-//   const [pageType, setPageType] = useState("initial");
-//   const [emailUser, setEmailUser] = useState()
-//   const dispatch = useDispatch();
-//   const navigate = useNavigate();
-//   const isNonMobile = useMediaQuery("(min-width:600px)");
-//   const isLogin = pageType === "login";
-//   const isRegister = pageType === "register";
-//   const isInitial = pageType === "initial";
-
-//   const register = async (values, onSubmitProps) => {
-//     // this allows us to send form info with image
-//     const formData = new FormData();
-//     for (let value in values) {
-//       formData.append(value, values[value]);
-//     }
-
-//     const savedUserResponse = await fetch(
-//       "http://localhost:3001/auth/register",
-//       {
-//         method: "POST",
-//         body: formData,
-//       }
-//     );
-//     const savedUser = await savedUserResponse.json();
-//     onSubmitProps.resetForm();
-
-//     if (savedUser) {
-//       setPageType("login");
-//     }
-//   };
-
-//   const login = async (values, onSubmitProps) => {
-//     try {
-//       const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify(values),
-//       });
-//       const loggedIn = await loggedInResponse.json();
-//       onSubmitProps.resetForm();
-//       if (loggedIn) {
-//         dispatch(
-//           setLogin({
-//             user: loggedIn.user,
-//             token: loggedIn.token,
-//           })
-//         );
-//         navigate("/");
-//       }
-
-//     } catch(err){
-//       return (err.message)
-//     }
-//   };
-
-//   const handleFormSubmit = async (values, onSubmitProps) => {
-//     setEmailUser(values.email)
-//     if (isInitial) {
-//       const result = await login(values, onSubmitProps)
-//       if(result.res.status === 200) {
-//         setPageType('login')
-//       } else setPageType('register')
-//     }
-//     if (isLogin) await login(values, onSubmitProps);
-//     if (isRegister) await register(values, onSubmitProps);
-//   };
-
-//   return (
-//     <Formik
-//       onSubmit={handleFormSubmit}
-//       initialValues={isLogin ? initialValuesLogin : initialValuesRegister}
-//       validationSchema={isLogin ? loginSchema : registerSchema}
-//     >
-//       {({
-//         values,
-//         errors,
-//         touched,
-//         handleBlur,
-//         handleChange,
-//         handleSubmit,
-//         resetForm,
-//       }) => (
-//         <form onSubmit={handleSubmit}>
-//           <Box
-//             display="grid"
-//             gap="30px"
-//             gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-//             sx={{
-//               "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
-//             }}
-//           >
-//             {isRegister && (
-//               <>
-//                 <TextField
-//                   label="First Name"
-//                   onBlur={handleBlur}
-//                   onChange={handleChange}
-//                   value={values.firstName}
-//                   name="firstName"
-//                   error={
-//                     Boolean(touched.firstName) && Boolean(errors.firstName)
-//                   }
-//                   helperText={touched.firstName && errors.firstName}
-//                   sx={{ gridColumn: "span 2" }}
-//                 />
-//                 <TextField
-//                   label="Last Name"
-//                   onBlur={handleBlur}
-//                   onChange={handleChange}
-//                   value={values.lastName}
-//                   name="lastName"
-//                   error={Boolean(touched.lastName) && Boolean(errors.lastName)}
-//                   helperText={touched.lastName && errors.lastName}
-//                   sx={{ gridColumn: "span 2" }}
-//                 />
-//                 <TextField
-//                   label="Birthday"
-//                   onBlur={handleBlur}
-//                   onChange={handleChange}
-//                   value={values.lastName}
-//                   name="birthday"
-//                   error={Boolean(touched.lastName) && Boolean(errors.lastName)}
-//                   helperText={touched.lastName && errors.lastName}
-//                   sx={{ gridColumn: "span 2" }}
-//                 />
-//               </>
-//             )}
-//             {isLogin && (
-//               <>
-//                 <TextField
-//                   label="Password"
-//                   type="password"
-//                   onBlur={handleBlur}
-//                   onChange={handleChange}
-//                   value={values.password}
-//                   name="password"
-//                   error={Boolean(touched.password) && Boolean(errors.password)}
-//                   helperText={touched.password && errors.password}
-//                   sx={{ gridColumn: "span 4" }}
-//                 />
-//               </>
-//             )}
-//             <TextField
-//               label="Email"
-//               onBlur={handleBlur}
-//               onChange={handleChange}
-//               value={values.email}
-//               name="email"
-//               error={Boolean(touched.email) && Boolean(errors.email)}
-//               helperText={touched.email && errors.email}
-//               sx={{ gridColumn: "span 4" }}
-//             />
-//           </Box>
-
-//           {/* BUTTONS */}
-//           <Box>
-//             <Button
-//               fullWidth
-//               type="submit"
-//               sx={{
-//                 m: "2rem 0",
-//                 p: "1rem",
-//               }}
-//             >
-//               {isInitial ? "Continue" : "Register now. Become a blue"} 
-//             </Button>
-//             {/* <Typography
-//               onClick={() => {
-//                 setPageType(isLogin ? "register" : "login");
-//                 resetForm();
-//               }}
-//               sx={{
-//                 textDecoration: "underline",
-//                 "&:hover": {
-//                   cursor: "pointer",
-//                 },
-//               }}
-//             >
-//               {isLogin
-//                 ? "Don't have an account? Sign Up here."
-//                 : "Already have an account? Login here."}
-//             </Typography> */}
-//           </Box>
-//         </form>
-//       )}
-//     </Formik>
-//   );
-// };
-
-// export default Form;
-
-
 import { useState } from "react";
 import { Box, Button, TextField } from "@mui/material";
 import { Formik } from "formik";
@@ -250,7 +21,12 @@ const registerSchema = yup.object({
   password: yup.string().required("Required"),
 });
 
-// Initial values
+const initialRegisterValues = {
+  firstName: "",
+  lastName: "",
+  password: "",
+};
+
 const initialEmailValues = {
   email: "",
 };
@@ -259,32 +35,23 @@ const initialLoginValues = {
   password: "",
 };
 
-const initialRegisterValues = {
-  firstName: "",
-  lastName: "",
-  password: "",
-};
-
 const Form = () => {
-  const [formStage, setFormStage] = useState("email"); // 'email', 'login', 'register'
+  const [formStage, setFormStage] = useState("email");
   const [userEmail, setUserEmail] = useState("");
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const handleEmailSubmit = async (values, { setSubmitting }) => {
-    // Check if email exists in the database
     const response = await fetch("http://localhost:3001/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email: values.email }),
     });
-  
+
     if (response.ok) {
-      // User exists, prompt for password
       setUserEmail(values.email);
       setFormStage("login");
     } else {
-      // No user found, switch to register form
       setUserEmail(values.email);
       setFormStage("register");
     }
@@ -329,10 +96,8 @@ const Form = () => {
     const data = await response.json();
 
     if (response.ok) {
-      // Registration successful, proceed to login
       setFormStage("login");
     } else {
-      // Handle registration failure
       console.error("Registration failed");
     }
 
@@ -363,7 +128,14 @@ const Form = () => {
           : handleRegisterSubmit
       }
     >
-      {({ values, errors, touched, handleChange, handleSubmit, isSubmitting }) => (
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleSubmit,
+        isSubmitting,
+      }) => (
         <form onSubmit={handleSubmit}>
           <Box display="flex" flexDirection="column" gap="20px">
             {formStage === "email" && (
@@ -416,8 +188,29 @@ const Form = () => {
                 />
               </>
             )}
-            <Button type="submit" disabled={isSubmitting}>
-              {formStage === "email" ? "Continue" : formStage === "login" ? "Login" : "Register"}
+            <Button
+              size="large"
+              type="submit"
+              disabled={isSubmitting}
+              sx={{
+                background: "linear-gradient(45deg, #673ab7, #03a9f4)",
+                borderRadius: "1.5rem",
+                color: "white",
+                padding: "1rem 2rem",
+                fontWeight: "bold",
+                textTransform: "none",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                transition: "background 0.3s ease",
+                "&:hover": {
+                  background: "#03a9f4",
+                },
+              }}
+            >
+              {formStage === "email"
+                ? "Continue"
+                : formStage === "login"
+                ? "Login"
+                : "Register"}
             </Button>
           </Box>
         </form>
--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,38 +1,41 @@
 import { Box, Typography, useMediaQuery, Avatar } from "@mui/material";
 import Form from "./Form";
+import Footer from "./footer";
 
 const LoginPage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
   return (
-    <Box>
+    <>
       <Box
-        width="100%"
-        backgroundColor= "white"
-        p="1rem 6%"
-        display= "flex"
-        justifyContent= "center"
+        height="90vh"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        backgroundColor="white"
       >
-        <Avatar src={`${process.env.PUBLIC_URL}/assets/logo.jpg`} alt="Club's Logo" 
-            sx={{ width: 100, height: 100 }}
+        <Avatar
+          src={`${process.env.PUBLIC_URL}/assets/logo.jpg`}
+          alt="Club's Logo"
+          sx={{ width: 100, height: 100, mb: 2 }}
         />
+        <Box
+          width={isNonMobileScreens ? "20%" : "93%"}
+          p="2rem"
+          borderRadius="1.5rem"
+          textAlign="center"
+        >
+          <Typography fontWeight="bold" variant="h1" sx={{ mb: "1.5rem" }}>
+            Bun venit!
+          </Typography>
+          <Typography variant="h5" sx={{ mb: "1.5rem" }}>
+            Loghează-te sau creazăți un cont nou:
+          </Typography>
+          <Form />
+        </Box>
       </Box>
-
-      <Box
-        width={isNonMobileScreens ? "50%" : "93%"}
-        p="2rem"
-        m="auto"
-        borderRadius="1.5rem"
-        textAlign= "center"
-      >
-        <Typography fontWeight="bold" variant="h4" sx={{ mb: "1.5rem" }}>
-          Bun venit!
-        </Typography>
-        <Typography variant="h6" sx={{ mb: "1.5rem" }}>
-          Loghează-te sau creazăți un cont nou: 
-        </Typography>
-        <Form />
-      </Box>
-    </Box>
+      <Footer />
+    </>
   );
 };
 
